827b16ec7696ddbf5d54502fa34ed2e8a4032a3d
hsux <72607204+closur3@users.noreply.github.com>
2025-06-18 12:31:43 +0800

    Update cncity.js

--- src/plugins/cncity.js
+++ src/plugins/cncity.js
@@ -4,23 +4,8 @@ const ipdb_range = require('@ipdb/range')
 const ipdb_cac = require('./cac')
 const ProgressBar = require('progress')
 
-const ISP_KEYWORDS = [
-  { domain: 'chinatelecom.com.cn', keyword: '电信', id: 'chinatelecom' },
-  { domain: 'chinaunicom.com',    keyword: '联通', id: 'chinaunicom' },
-  { domain: 'chinamobile.com',    keyword: '移动', id: 'chinamobile' }
-]
-
-function isThreeMainISPs(info) {
-  // 明确的isp_domain
-  if (info.isp_domain) {
-    return ISP_KEYWORDS.some(i => info.isp_domain.includes(i.domain))
-  }
-  // 其他字段模糊判断
-  const fields = [info.isp, info.owner, info.org, info.name].filter(Boolean).join('|')
-  return ISP_KEYWORDS.some(i => fields.includes(i.keyword))
-}
-
 const plugin = (through2, file, cb) => {
+
   console.log('Parse ipdb')
 
   const ipdb = new IPDB(file.contents, {
@@ -34,9 +19,7 @@ const plugin = (through2, file, cb) => {
   while (true) {
     const info = ipdb.find(ip).data
     const china_admin_code = info.china_admin_code
-
-    // 只保留三大运营商
-    if (china_admin_code?.length === 6 && isThreeMainISPs(info)) {
+    if (china_admin_code?.length === 6) {
       let cac = china_admin_code
       {
         cac = `${cac.substr(0, 4)}00`
