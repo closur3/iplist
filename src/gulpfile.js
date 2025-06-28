8cfe3723a71cf6325f87e2af802bafacf009ebf3
hsux <72607204+closur3@users.noreply.github.com>
2025-06-28 14:05:20 +0800

    Update gulpfile.js

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,31 +1,10 @@
 const { series, src, dest } = require('gulp')
 const through2 = require('through2')
-const plugin_country = require('./plugins/country')
-const plugin_city = require('./plugins/city')
 const plugin_cncity = require('./plugins/cncity')
-const plugin_china = require('./plugins/china')
-const plugin_cidrmerge = require('./plugins/cidrmerge')
 const plugin_isp = require('./plugins/isp')
+const plugin_cidrmerge = require('./plugins/cidrmerge')
 
 const database = '/tmp/openipdb.ipdb'
-
-const country = () => {
-  return src(database)
-    .pipe(through2.obj(function(file, _, cb) {
-      return plugin_country(this, file, cb)
-    }))
-    .pipe(through2.obj(plugin_cidrmerge))
-    .pipe(dest('data/country'))
-}
-
-const city = () => {
-  return src(database)
-    .pipe(through2.obj(function(file, _, cb) {
-      return plugin_city(this, file, cb)
-    }))
-    .pipe(through2.obj(plugin_cidrmerge))
-    .pipe(dest('data/country'))
-}
 
 const cncity = () => {
   return src(database)
@@ -45,17 +24,6 @@ const isp = () => {
     .pipe(dest('data/isp'))
 }
 
-const china = () => {
-  return src('data/country/CN.txt')
-    .pipe(through2.obj(function(file, _, cb) {
-      return plugin_china(this, file, cb)
-    }))
-    .pipe(dest('data/special'))
-}
-
-exports.country = country
-exports.city = city
 exports.cncity = cncity
-exports.china = china
 exports.isp = isp
-exports.build = series(country, city, cncity, isp, china)
+exports.build = series(cncity, isp)
